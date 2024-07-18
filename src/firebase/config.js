a11d735e9407e54313de8d116872b2b8d3b15dd6
Augustine Rita <egwuaturita95@gmail.com>
2024-07-18 09:00:46 +0100

    Add files via upload
    file

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBzAyw4M_sidjEZaLMeHtdFqALLsgHDMeI",
+    authDomain: "the-grills.firebaseapp.com",
+    projectId: "the-grills",
+    storageBucket: "the-grills.appspot.com",
+    messagingSenderId: "337040250646",
+    appId: "1:337040250646:web:dfbe7646612a6f8e0b2237",
+    measurementId: "G-BMYTPR73QJ"
+  };
+
+  export default firebaseConfig;
