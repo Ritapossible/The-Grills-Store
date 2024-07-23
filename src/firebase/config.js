30053d3b264d0fcea269a6c938a0b4415289a9ab
Ritapossible <egwuaturita95@gmail.com>
2024-07-23 10:02:42 -0700

    config file

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,11 @@
-const firebaseConfi {
-    apiKey: "AIzaSyBzAyw4M_sidjEZaLMeHtdFqALLsgHDMeI",
-    authDomain: "the-grills.firebaseapp.com",
-    projectId: "the-grills",
-    storageBucket: "the-grills.appspot.com",
-    messagingSenderId: "337040250646",
-    appId: "1:337040250646:web:dfbe7646612a6f8e0b2237",
-    measurementId: "G-BMYTPR73QJ"
+const firebaseConfig {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   export default firebaseConfig;
